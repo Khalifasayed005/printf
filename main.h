99dbf78a901fdf9a46e68d111138685a120a6faf
Khalifasayed005 <khalifa.sayed.005@gmail.com>
2023-09-19 02:00:21 -0700

    My second commit

--- main.h
+++ main.h
@@ -20,12 +20,15 @@
 
 /**
  * struct parameters - parameters struct
+ *
  * @unsign: flag if unsigned value
+ *
  * @plus_flag: on if plus_flag specified
  * @space_flag: on if hashtag flag specified
  * @hashtag_flag: on if_flag specified
  * @zero_flag: on if_flag specified
  * @minus_flag: on if_flag specified
+ *
  * @width: field width specified
  * @precision: field precision specifieda
  * @h_modifier: on if h_modifier is specified
@@ -33,38 +36,36 @@
 */
 typedef struct parameters
 {
-unsigned int unsign		: 1;
+	unsigned int unsign		: 1;
 
-unsigned int plus_flag		: 1;
-unsigned int space_flag		: 1;
-unsigned int hashtag_flag	: 1;
-unsigned int zero_flag		: 1;
-unsigned int minus_flag		: 1;
+	unsigned int plus_flag		: 1;
+	unsigned int space_flag		: 1;
+	unsigned int hashtag_flag	: 1;
+	unsigned int zero_flag		: 1;
+	unsigned int minus_flag		: 1;
 
-unsigned int width;
-unsigned int precision;
+	unsigned int width;
+	unsigned int precision;
 
-unsigned int h_modifier		: 1;
-unsigned int l_modifier		: 1;
+	unsigned int h_modifier		: 1;
+	unsigned int l_modifier		: 1;
 } params_t;
 
 /**
 * struct specifier - Struct token
+*
 * @specifier: format token
 * @f: The function associated
 */
-
 typedef struct specifier
 {
 char *specifier;
-
 int (*f)(va_list, params_t *);
 } specifier_t;
 
 /* _put.c module */
 
 int _puts(char *str);
-
 int _putchar(int c);
 
 /* print_functions.c module */
